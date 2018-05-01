23dd2205b8b699c46e82d62956a988ace82e201c
meng-jie <1217435660@qq.com>
2018-05-01 17:10:47 +0800

    Add files via upload

---
+++ Web/js/common.js
@@ -0,0 +1,39 @@
+/**
+ * Created by jiangyangyang on 2018/4/27.
+ */
+var jkcommon =
+{
+    init:function()
+    {
+        this.lessImgShow();
+    },
+    stopEventBubble:function(event)
+    {
+        var e = event;
+          if(e)
+          {
+              e.stopPropagation();
+          }
+    },
+    lessImgShow:function()
+    {
+        $('.lessonimagebox').bind("mouseover",function(e)
+        {
+            var _pop = $(this).find('.img-pop');
+            $(this).css({border:"2px solid #1abc9c",width:"226px" ,height:"226px"});
+            TweenMax.to(_pop,0.5,{top:0,ease:Bounce.easeOut});
+            jkcommon.stopEventBubble(e);
+        });
+        $(document).bind("mouseover",function()
+        {
+            $('.lessonimagebox').css({border:"0",width:"230px",height:"230px"});
+            var _pop = $(this).find(".img-pop");
+            TweenMax.to(_pop,0.5,{top:230,ease:Linear.easeNone});
+        })
+    }
+}
+
+$(function()
+{
+    jkcommon.init();
+})
